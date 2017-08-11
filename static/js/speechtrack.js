ac9b3b69e992d3392086225827f5ba2abbcaa1ca
Aku Rouhe <aku.rouhe@aalto.fi>
2017-08-11 12:33:10 +0300

    Fade past and future words, blink cursor

--- static/js/speechtrack.js
+++ static/js/speechtrack.js
@@ -1,38 +1,58 @@
 /* State: */
 var speechTracking = false;
 var workerAvailable = false;
+var cursorPos = 0;
 var ReadingPrompt = function() {
   var asList = [];
-  var asSpanList = [];
+  var asHTMLList = [];
+  var lastSpace = null;
   this.loadText = function(text) {
     asList = text.split(/(\s+)/).filter( function(x) {return x.trim().length > 0;});
-    asSpanList = [];
+    asHTMLList = [];
     for (i = 0; i < asList.length; i++) {
+      //var wordDiv = document.createElement('div');
+      var spaceSpan = document.createElement('span');
       var wordSpan = document.createElement('span');
-      wordSpan.innerHTML = asList[i] + " ";
+      spaceSpan.innerHTML = " ";
+      spaceSpan.style["white-space"] = 'pre';
+      wordSpan.innerHTML = asList[i];
       wordSpan.classList.add('promptword');
-      asSpanList.push(wordSpan);
+      //wordDiv.appendChild(spaceSpan);
+      //wordDiv.appendChild(wordSpan);
+      //wordDiv.classList.add('promptword');
+      asHTMLList.push({//div: wordDiv, 
+        spaceSpan: spaceSpan,
+        wordSpan: wordSpan});
     }
+    lastSpace = document.createElement('span');
+    lastSpace.innerHTML = " ";
+    lastSpace.style["white-space"] = 'pre';
   }
-  this.getSpanList = function() {
-    return asSpanList;
+  this.getHTMLList = function() {
+    return asHTMLList;
   }
-  this.getElementAtIndex = function(index) {
-    return asSpanList[index];
+  this.getWordAtIndex = function(index) {
+    return asHTMLList[index].wordSpan;
+  }
+  this.getLastSpace = function() {
+    return lastSpace;
   }
 };
-function setPrompt(spanList) {
+function setPromptDiv(readingPrompt) {
+  var HTMLList = readingPrompt.getHTMLList();
+  var lastSpace = readingPrompt.getLastSpace();
   var promptdiv = document.getElementById("prompt")
   promptdiv.innerHTML = "";
-  for (i = 0; i < spanList.length; i++) {
-    promptdiv.appendChild(spanList[i]);
+  for (i = 0; i < HTMLList.length; i++) {
+    promptdiv.appendChild(HTMLList[i].spaceSpan);
+    promptdiv.appendChild(HTMLList[i].wordSpan);
   }
+  promptdiv.appendChild(lastSpace);
 }
 
 var readingPrompt = new ReadingPrompt();
 readingPrompt.loadText("En dag började solen och vinden bråka om vem av dem som var starkast.")
-setPrompt(readingPrompt.getSpanList());
-
+setPromptDiv(readingPrompt);
 
 var dictate = new Dictate({
   server : "ws://localhost:8888/client/ws/speech",
@@ -57,8 +77,10 @@ var dictate = new Dictate({
 
 /* callbacks for dictate */
 function __processHypothesis(hypothesis) {
+  console.log("Got hypo");
   parsedHypo = parseHypothesis(hypothesis);
   colorPrompt(readingPrompt, parsedHypo);
+  blinkCursor();
 }
 
 function colorPromptByHypothesis(readingPrompt, hypothesis) {
@@ -66,27 +88,93 @@ function colorPromptByHypothesis(readingPrompt, hypothesis) {
   var reresult; 
   while ((reresult = reWordIds.exec(hypothesis)) !== null) {
     index = parseInt(reresult[1]);
-    readingPrompt.getElementAtIndex(index).classList.add('correct');
+    readingPrompt.getWordAtIndex(index).classList.add('correct');
   }
 }
 
-function colorPrompt(readingPrompt, parsedHypothesis) {
-  for (let index of parsedHypo.correctInds) {
-    readingPrompt.getElementAtIndex(index).classList.add('correct');
-    readingPrompt.getElementAtIndex(index).classList.remove('truncated');
-    readingPrompt.getElementAtIndex(index).classList.remove('missed');
-  }
-  for (let index of parsedHypo.truncInds) {
-    readingPrompt.getElementAtIndex(index).classList.remove('correct');
-    readingPrompt.getElementAtIndex(index).classList.add('truncated');
-    readingPrompt.getElementAtIndex(index).classList.remove('missed');
-  }
-  for (let index of parsedHypo.missedInds) {
-    readingPrompt.getElementAtIndex(index).classList.remove('correct');
-    readingPrompt.getElementAtIndex(index).classList.remove('truncated');
-    readingPrompt.getElementAtIndex(index).classList.add('missed');
+function setReadingClass(element, cls) {
+  element.classList.remove('correct');
+  element.classList.remove('truncated');
+  element.classList.remove('missed');
+  if (cls !== null) {
+    element.classList.add(cls);
   }
 }
+function getReadingClassByIndex(parsedHypo, index) {
+  if (parsedHypo.correctInds.has(index)) {return 'correct';}
+  if (parsedHypo.truncInds.has(index)) {return 'truncated';}
+  if (parsedHypo.missedInds.has(index)) {return 'missed';}
+  return null;
+}
+
+function colorPrompt(readingPrompt, parsedHypothesis) {
+  var HTMLList = readingPrompt.getHTMLList();
+  for (index = 0; index < HTMLList.length; index++) {
+    cls = getReadingClassByIndex(parsedHypothesis, index);
+    setReadingClass(HTMLList[index].wordSpan, cls);
+    if (speechTracking) {
+      HTMLList[index].wordSpan.classList.remove('future');
+      HTMLList[index].wordSpan.classList.remove('past');
+      if (index >= parsedHypothesis.cursor + 1) {
+        HTMLList[index].wordSpan.classList.add('future');
+      }
+      if (index < parsedHypothesis.cursor) {
+        HTMLList[index].wordSpan.classList.add('past');
+      }
+    }
+  }
+  cursorPos = parsedHypothesis.cursor;
+}
+
+function colorAtStart(readingPrompt) {
+  var HTMLList = readingPrompt.getHTMLList();
+  for (index = 1; index < HTMLList.length; index++) {
+    HTMLList[index].wordSpan.classList.add('future');
+  }
+  cursorPos = 0;
+}
+function colorAtEnd(readingPrompt) {
+  var HTMLList = readingPrompt.getHTMLList();
+  for (index = 0; index < HTMLList.length; index++) {
+    HTMLList[index].wordSpan.classList.remove('future');
+    HTMLList[index].wordSpan.classList.remove('past');
+  }
+}
+
+var cursorOn = false;
+function blinkCursor() {
+  var HTMLList = readingPrompt.getHTMLList();
+  for (i = 0; i < HTMLList.length; i++) {
+    if (cursorPos === i) {
+      if (speechTracking && !cursorOn) {
+        cursorOn = true;
+        HTMLList[i].spaceSpan.classList.add('blink');
+      }
+      else {
+        HTMLList[i].spaceSpan.classList.remove('blink');
+        cursorOn = false;
+      }
+    }
+    else {
+      HTMLList[i].spaceSpan.classList.remove('blink');
+    }
+  }
+  var lastSpace = readingPrompt.getLastSpace();
+  if (cursorPos === HTMLList.length) {
+    if (speechTracking && !cursorOn) {
+      cursorOn = true;
+      lastSpace.classList.add('blink');
+    }
+    else {
+      lastSpace.classList.remove('blink');
+      cursorOn = false;
+    }
+  } 
+  else {
+    lastSpace.classList.remove('blink');
+  }
+}
+window.setInterval(blinkCursor, 200);
 
 function parseHypothesis(hypothesis) {
   var parsed = {correctInds: new Set(), truncInds: new Set(), missedInds: new Set(), cursor: 0};
@@ -97,7 +185,7 @@ function parseHypothesis(hypothesis) {
   while ((reresult = reCorrectInds.exec(hypothesis)) !== null) {
     index = parseInt(reresult[1]);
     parsed.correctInds.add(index);
-    parsed.cursor = index;
+    parsed.cursor = index + 1;
     triedWords.push(index);
   }
   while ((reresult = reTruncInds.exec(hypothesis)) !== null) {
@@ -129,9 +217,12 @@ document.onkeydown = function(evt) {
     try{
       dictate.startListening();
       speechTracking = true;
+      __processHypothesis(""); 
+      colorAtStart(readingPrompt);
     } catch (e) {
       console.log("Unable to start speech tracking due to error: " +e);
       speechTracking = false;
+      colorAtEnd(readingPrompt);
     }
   }
 };
@@ -141,6 +232,7 @@ document.onkeyup = function(evt) {
     console.log("Stopping speech tracking.");
     dictate.stopListening();
     speechTracking = false;
+    colorAtEnd(readingPrompt);
   }
 };
 
